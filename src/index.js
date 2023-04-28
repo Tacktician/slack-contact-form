3e7f112cd95bf60f3ba32bdd9b2737c6b8d74c1c
Paloma Oliveira <paloma.oliveira@saucelabs.com>
2023-04-28 18:16:08 +0200

    Add styles to your be-au-ti-ful form (#1)
    * Fix flex
    
    * Add font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactModal from './components/ContactModal';
+import './index.css';
 
 ReactDOM.render(<ContactModal />, document.getElementById('root'));
